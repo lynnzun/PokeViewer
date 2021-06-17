f2de56204c0c2bf88ca0af4a895e07708bf54efe
Marek <marex88@poczta.fm>
2021-06-17 10:06:34 +0200

    Add project files.

---
+++ PokeViewer/Controllers/HomeController.cs
@@ -0,0 +1,74 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using PokeViewer.Models;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Net.Http;
+using System.Text.Json;
+using System.Threading.Tasks;
+
+namespace PokeViewer.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            //var Pokemons = GetPokemonsFromMemory();
+            var Pokemons = GetPokemonsFromAPIAsync().Result;
+            return View(Pokemons);
+        }
+
+        private object GetPokemonsFromMemory()
+        {
+            var Pokemons = new List<Pokemon>();
+            Pokemons.Add(new Pokemon { PokemonId = 0, Name = "Nazwa", Height = 5, Weight = 10 });
+            Pokemons.Add(new Pokemon { PokemonId = 152, Name = "Nowa nazwa", Height = 4, Weight = 12 });
+            
+            return Pokemons;
+        }
+
+        private async Task<object> GetPokemonsFromAPIAsync()
+        {
+            var Pokemons = new List<Pokemon>();
+            var client = new HttpClient();
+            var request = new HttpRequestMessage
+            {
+                Method = HttpMethod.Get,
+                RequestUri = new Uri("https://pokeapi.co/api/v2/pokemon/151")
+            };
+
+            using (var response = await client.SendAsync(request))
+            {
+                response.EnsureSuccessStatusCode();
+                var body = await response.Content.ReadAsStringAsync();
+                var pokemon = new Pokemon();
+
+
+                pokemon = JsonSerializer.Deserialize<Pokemon>(body);
+                Pokemons.Add(pokemon);
+            }
+            return Pokemons;
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+
+    }
+}
---
+++ PokeViewer/Models/Pokemon.cs
@@ -0,0 +1,27 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text.Json.Serialization;
+using System.Threading.Tasks;
+
+namespace PokeViewer.Models
+{
+    public class Pokemon
+    {
+        [JsonPropertyName("id")]
+        public int PokemonId { get; set; }
+
+        [JsonPropertyName("name")]
+        public string Name { get; set; }
+
+        [JsonPropertyName("height")]
+        public int Height { get; set; }
+
+        [JsonPropertyName("weight")]
+        public int Weight { get; set; }
+
+        [JsonInclude]
+        public string SpritesFront_default { get; set; }
+    }
+}
