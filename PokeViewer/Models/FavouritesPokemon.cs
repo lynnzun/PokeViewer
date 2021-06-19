ed01f6acc7268f823b0ce006e8a50c1757556300
Marek <marex88@poczta.fm>
2021-06-19 14:17:07 +0200

    Favourites added

---
+++ PokeViewer/Models/FavouritesPokemon.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace PokeViewer.Models
+{
+    public class FavouritesPokemon : Pokemon
+    {
+        public string Owner { get; set; }
+        public string PersonalName { get; set; }
+
+    }
+}
